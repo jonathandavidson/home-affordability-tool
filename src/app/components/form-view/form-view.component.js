bf21cd13bb29fad329249b28235457bf6993632a
Jonathan Davidson <jdavidson@netsplasher.com>
2016-07-20 07:58:54 -0500

    Linked form data to controller on submission

--- src/app/components/form-view/form-view.component.js
+++ src/app/components/form-view/form-view.component.js
@@ -1,7 +1,10 @@
+import FormViewController from './form-view.controller';
+
 const template = require('./form-view.html');
 
 require('./form-view.scss');
 
 export default {
+  controller: FormViewController,
   template,
 };
---
+++ src/app/components/form-view/form-view.controller.js
@@ -0,0 +1,4 @@
+export default class FormViewController {
+  processForm(current, desired) {
+  }
+}
