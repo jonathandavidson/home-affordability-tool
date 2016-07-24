9a13964be3962636cb5803f5edf1814319217702
Jonathan Davidson <jdavidson@netsplasher.com>
2016-07-24 14:54:08 -0500

    Added basic form validation

--- src/app/components/form-view/form-view.controller.js
+++ src/app/components/form-view/form-view.controller.js
@@ -1,4 +1,11 @@
 export default class FormViewController {
+  constructor() {
+    this.patterns = {
+      currency: /^\d+(\.\d{2})?$/,
+      percent: /^\d{1,2}(?!\d)|100$/,
+    };
+  }
+
   processForm(current, desired) {
   }
 }
