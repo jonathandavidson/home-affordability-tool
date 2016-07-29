3f736a7ddb15601cf3ee39b81c72fd9235871172
Jonathan Davidson <jdavidson@netsplasher.com>
2016-07-29 08:30:08 -0500

    Added routes

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,25 @@
 import angular from 'angular';
+import uiRouter from 'angular-ui-router';
 import AppComponent from './app.component';
 import Components from './components';
 
-angular.module('app', [Components])
-  .component('app', AppComponent);
+angular.module('app', [
+  Components,
+  uiRouter,
+])
+  .component('app', AppComponent)
+  .config(($stateProvider, $urlRouterProvider, $locationProvider) => {
+    $locationProvider.html5Mode(true);
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('form', {
+      url: '/',
+      template: '<form-view />',
+    });
+
+    $stateProvider.state('results', {
+      url: '/results',
+      template: '<results />',
+    });
+  });
--- src/app/components/form-view/index.js
+++ src/app/components/form-view/index.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
+import uiRouter from 'angular-ui-router';
 import FormView from './form-view.component';
 
-export default angular.module('formView', [])
+export default angular.module('formView', [uiRouter])
   .component('formView', FormView)
   .name;
