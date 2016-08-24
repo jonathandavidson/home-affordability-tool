53dac81b422cefa9ea6702125cd4034a0989b5d4
Jonathan Davidson <jdavidson@netsplasher.com>
2016-08-24 07:39:46 -0500

    Added unit tests for form-vew.service

--- src/app/components/form-view/__tests__/form-view.service.spec.js
+++ src/app/components/form-view/__tests__/form-view.service.spec.js
@@ -1,19 +1,31 @@
 import FormViewService from '../form-view.service';
 
-describe('form-view.service', () => {
-  const commonInputs = {
-    monthlyAfterTaxIncome: 5000,
-    downPaymentPercent: 20,
-    homeValue: 100000,
-    loanTerm: 30,
-    loanValue: 80000,
-    paymentPercentOfIncome: 25,
-    savingsBalance: 2000,
-    sellingCosts: 6000,
-  };
+const commonInputs = {
+  monthlyAfterTaxIncome: 5000,
+  downPaymentPercent: 20,
+  homeValue: 100000,
+  loanTerm: 30,
+  loanValue: 80000,
+  paymentPercentOfIncome: 25,
+  savingsBalance: 2000,
+  sellingCosts: 6000,
+};
 
-  describe('when calculate is called with valid inputs', () => {
-    it('should return a promise that resolves with the correct outputs', done => {
+const formValues = {
+  foo: 'fooValue',
+  bar: 'barValue',
+};
+
+const $window = {
+  localStorage: {
+    setItem: jasmine.createSpy(),
+    getItem: jasmine.createSpy(),
+  },
+};
+
+describe('form-view.service', () => {
+  describe('when calculate() is called', () => {
+    it('returns a promise that resolves with the correct outputs', done => {
       const inputs = Object.assign({}, commonInputs);
       const service = new FormViewService({});
 
@@ -38,6 +50,30 @@ describe('form-view.service', () => {
       service.calculate(inputs)
         .then(testResolve)
         .catch(failTest);
+    });
+  });
+
+  describe('when setFormValues() is called', () => {
+    const service = new FormViewService($window);
+
+    service.setFormValues(formValues);
+    it('saves the serialized object in localStorage', () => {
+      expect($window.localStorage.setItem.calls.count())
+        .toEqual(1);
+
+      expect($window.localStorage.setItem)
+        .toHaveBeenCalledWith('formValues', JSON.stringify(formValues));
+    });
+  });
+
+  describe('when getFormValues() is called', () => {
+    $window.localStorage.getItem.and.returnValue(JSON.stringify(formValues));
+    const service = new FormViewService($window);
+
+    const returnValue = service.getFormValues(formValues);
+
+    it('returns the unserialized object from localStorage', () => {
+      expect(returnValue).toEqual(formValues);
     });
   });
 });
